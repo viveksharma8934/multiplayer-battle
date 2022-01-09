625d0c464d2050cfd0df6468a68a748e4d288e0c
Viveksharma8934 <viveksharma8934@gmail.com>
2022-01-09 13:25:05 +0530

    added-navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Carousel from './components/Carousel/Carousel';
-import {React,Component} from 'react'
+import { React, Component } from 'react'
+import ResponsiveAppBar from './components/Navbar/Navbar';
 
 
 export default class App extends Component {
   render() {
     return (
       <div>
+        <ResponsiveAppBar />
         <Carousel />
       </div>
     )
--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,4 +1,4 @@
-import React,{Component} from 'react'
+import React from 'react'
 import M from 'materialize-css';
 import 'materialize-css/dist/css/materialize.min.css'
 import GameCard from '../GameCard/GameCard.js'
